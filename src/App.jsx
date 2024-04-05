83b116f3b2af5489f8b49fcf0aebcdd93b3af742
Soulbroda23 <ogundeyi.dieko13@gmail.com>
2024-04-05 06:00:59 -0600

    worked on  the dashboard, addding backgrounds, creating buttons the create poll and view poll and also the profile button and menu bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,69 +1,85 @@
-import { useState, useEffect, } from 'react';
-import { Link, createBrowserRouter, createRoutesFromElements, RouterProvider, Route } from "react-router-dom";
-import SignUpPage, { action as signUpAction, loader as signUpLoginLoader } from './pages/SignUp';
-import SignInPage, { action as signInAction, loader as signInLoginLoader } from './pages/SignIn';
-import {  connectFirestoreEmulator, getFirestore, } from "firebase/firestore";
-import { onAuthStateChanged, getAuth, connectAuthEmulator, } from "firebase/auth";
+import { useState, useEffect } from "react";
+import {
+	Link,
+	createBrowserRouter,
+	createRoutesFromElements,
+	RouterProvider,
+	Route,
+} from "react-router-dom";
+import SignUpPage, {
+	action as signUpAction,
+	loader as signUpLoginLoader,
+} from "./pages/SignUp";
+import SignInPage, {
+	action as signInAction,
+	loader as signInLoginLoader,
+} from "./pages/SignIn";
+import { connectFirestoreEmulator, getFirestore } from "firebase/firestore";
+import {
+	onAuthStateChanged,
+	getAuth,
+	connectAuthEmulator,
+} from "firebase/auth";
+import Dashboard from "./pages/Dashboard";
 import { app } from "./firebase";
 
-
 export default function App() {
+	const [userId, setUserId] = useState(null);
 
-  const [userId, setUserId] = useState(null);
+	const db = getFirestore();
 
-  const db = getFirestore();
+	// localStorage.clear();
 
-  // localStorage.clear();
+	useEffect(() => {
+		const auth = getAuth(app);
+		const observer = onAuthStateChanged(auth, (user) => {
+			if (user) {
+				setUserId(user.uid);
+			} else {
+				setUserId(null);
+			}
+		});
 
-  useEffect(() => {
-    const auth = getAuth(app);
-    const observer = onAuthStateChanged(auth, user => {
-      if (user) {
-        setUserId(user.uid);
-      } else {
-        setUserId(null);
-      }
-    });
+		// if (location.hostname === "localhost") {
+		// 	connectAuthEmulator(auth, "http://localhost:9099");
+		// 	connectFirestoreEmulator(db, "localhost", "5174");
+		// }
 
-    if (location.hostname === "localhost") {
-      connectAuthEmulator(auth, "http://localhost:9099");
-      connectFirestoreEmulator(db, "localhost", "5174");
-    }
+		return () => observer();
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, []);
 
-    return () => observer();
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-  
-  const router = createBrowserRouter(
-    createRoutesFromElements(
-      <>
-        <Route 
-          path="/" 
-          element={
-            <>
-              <h1>Hello World</h1>
-            </>
-          }
-        />
-        <Route 
-          path="/signup" 
-          element={<SignUpPage />} 
-          action={signUpAction}
-          loader={signUpLoginLoader}
-        />
-        <Route 
-          path="/signin" 
-          element={<SignInPage />} 
-          action={signInAction}
-          loader={signInLoginLoader}
-        />
-      </>
-    )
-  );
+	const router = createBrowserRouter(
+		createRoutesFromElements(
+			<>
+				<Route
+					path="/"
+					element={
+						<>
+							<h1>Hello World</h1>
+						</>
+					}
+				/>
+				<Route
+					path="/signup"
+					element={<SignUpPage />}
+					action={signUpAction}
+					loader={signUpLoginLoader}
+				/>
+				<Route
+					path="/signin"
+					element={<SignInPage />}
+					action={signInAction}
+					loader={signInLoginLoader}
+				/>
+				<Route path="/dashboard" element={<Dashboard />} />
+			</>
+		)
+	);
 
-  return (
-    <>
-      <RouterProvider router={router} />
-    </>
-  )
+	return (
+		<>
+			<RouterProvider router={router} />
+		</>
+	);
 }
