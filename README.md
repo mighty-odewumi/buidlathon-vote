f449cee546a4a94c4c4d69b55491d7888a57b847
gr1ntch <odewumimighty@gmail.com>
2024-04-05 11:15:30 +0100

    Modified README and started Dashboard

--- README.md
+++ README.md
@@ -1,8 +1,11 @@
-# 9ja Vote
+# UniPoll
 
-Digital Voting Platform
+Digital Voting Platform for Universties
 
 ## Installation
 
-
+* Clone the repo from ```https://github.com/mighty-odewumi/buidlathon-vote.git```.
+* Change into the directory you cloned into with ```cd buidlathon-vote```.
+* Run ```npm install``` to download the dependencies.
+* Run ```npm run dev``` to start up the server.
 
---
+++ src/pages/Dashboard.jsx
@@ -0,0 +1,27 @@
+import { Link } from "react-router-dom";
+import { menu } from "../assets/menu.svg";
+import { profile } from "../assets/profile.svg";
+import { background } from "../assets/bg-img.svg";
+
+
+export default function Dashboard() {
+  return (
+    <>
+      <div className="">
+        <header>
+          <img 
+            src={menu} 
+            alt="Menu" 
+          />
+
+          <img 
+            src={profile} 
+            alt="Profile" 
+          />
+        </header>
+        <h1>Welcome to UniPoll! Your voice counts here.</h1>
+        <Link to=""></Link>
+      </div>
+    </>
+  )
+}
